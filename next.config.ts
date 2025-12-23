6bbf21dc504f8a43835c42096061799df01c7f3b
AiFahri <fahrinurilll@gmail.com>
2025-12-23 12:09:24 +0700

    fix: standardize quotes in next.config.ts for consistency

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: "standalone",
   images: {
+    remotePatterns: [{ protocol: "https", hostname: "cdn.sanity.io" }],
     unoptimized: true,
   },
 };
